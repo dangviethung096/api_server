f92b25baf74c4f940e51df7c57b66203f078f066
vitcon <dangviethung096@gmail.com>
2023-09-28 22:59:32 +0700

    update header

---
+++ search_flight/amadeus/header.go
@@ -0,0 +1,77 @@
+package amadeus
+
+import "encoding/xml"
+
+type Envelope struct {
+	XMLName xml.Name    `xml:"soapenv:Envelope"`
+	Env     *string     `xml:"xmlns:soapenv,attr"`
+	Sec     *string     `xml:"xmlns:sec,attr"`
+	Link    *string     `xml:"xmlns:link,attr"`
+	Ses     *string     `xml:"xmlns:ses,attr"`
+	Header  *Header     `xml:"soapenv:Header"`
+	Body    interface{} `xml:"soapenv:Body"`
+}
+
+type Header struct {
+	Wsa                   *string                `xml:"xmlns:wsa,attr"`
+	Typ                   *string                `xml:"xmlns:typ,attr"`
+	Iat                   *string                `xml:"xmlns:iat,attr"`
+	AMASecurityHostedUser *AMASecurityHostedUser `xml:"sec:AMA_SecurityHostedUser"`
+	Security              *Security              `xml:"wsse:Security"`
+	To                    *string                `xml:"wsa:To"`
+	Action                *string                `xml:"wsa:Action"`
+	MessageID             *string                `xml:"wsa:MessageID"`
+	TransactionFlowLink   *TransactionFlowLink   `xml:"link:TransactionFlowLink"`
+	Session               *Session               `xml:"ses:Session"`
+}
+
+type AMASecurityHostedUser struct {
+	UserID *UserID `xml:"sec:UserID"`
+}
+
+type UserID struct {
+	PosType        *string      `xml:"POS_Type,attr"`
+	RequestorType  *string      `xml:"RequestorType,attr"`
+	PseudoCityCode *string      `xml:"PseudoCityCode,attr"`
+	AgentDutyCode  *string      `xml:"AgentDutyCode,attr"`
+	RequestorID    *RequestorID `xml:"typ:RequestorID"`
+}
+
+type RequestorID struct {
+	CompanyName *string `xml:"iat:CompanyName"`
+}
+
+type Security struct {
+	Wsse          *string        `xml:"xmlns:wsse,attr"`
+	Wsu           *string        `xml:"xmlns:wsu,attr"`
+	UsernameToken *UsernameToken `xml:"wsse:UsernameToken"`
+}
+
+type UsernameToken struct {
+	Username *string   `xml:"wsse:Username"`
+	Password *Password `xml:"wsse:Password"`
+	Nonce    *Nonce    `xml:"wsse:Nonce"`
+	Created  *string   `xml:"wsu:Created"`
+}
+
+type Password struct {
+	Type *string `xml:"Type,attr"`
+	Text *string `xml:",chardata"`
+}
+
+type Nonce struct {
+	EncodingType *string `xml:"EncodingType,attr"`
+	Text         *string `xml:",chardata"`
+}
+
+type Session struct {
+	TransactionStatusCode *string `xml:"TransactionStatusCode,attr"`
+}
+
+type TransactionFlowLink struct {
+	Consumer *Consumer `xml:"link:Consumer"`
+}
+
+type Consumer struct {
+	UniqueID *string `xml:"link:UniqueID"`
+}
--- search_flight/amadeus/requests_test.go
+++ search_flight/amadeus/requests_test.go
@@ -6,27 +6,81 @@ import (
 	"testing"
 )
 
+var (
+	Env                   = "http://schemas.xmlsoap.org/soap/envelope/"
+	Sec                   = "http://schemas.xmlsoap.org/soap/envelope/"
+	Link                  = "http://xml.amadeus.com/2010/06/Security_v1"
+	Ses                   = "http://wsdl.amadeus.com/2010/06/ws/Link_v1"
+	Wsa                   = "http://www.w3.org/2005/08/addressing"
+	Typ                   = "http://xml.amadeus.com/2010/06/Types_v1"
+	Iat                   = "http://www.iata.org/IATA/2007/00/IATA2010.1"
+	PosType               = "1"
+	RequestorType         = "U"
+	PseudoCityCode        = "HANVN08AA"
+	AgentDutyCode         = "SU"
+	CompanyName           = "VN"
+	Wsse                  = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd"
+	Wsu                   = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd"
+	Username              = "WSVNGEN"
+	PasswordType          = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordDigest"
+	PasswordText          = "Irwhru5lS3mwKMyUods/p72MWDE="
+	EncodingType          = "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary"
+	NonceText             = "Z0ZHQWh1bmx6Vnpu"
+	Created               = "2022-11-15T17:00:10.217Z"
+	To                    = "https://nodeA1.test.webservices.amadeus.com/1ASIWGENVN"
+	Action                = "http://webservices.amadeus.com/SATRQT_19_1_1A"
+	MessageID             = "urn:uuid:e88e9b9d-7a0f-46e0-8f01-e0cf6325cb67"
+	UniqueID              = "oef5izfzuaawy3ergzk5005h"
+	TransactionStatusCode = "Start"
+)
+
 func TestXmlRequest(t *testing.T) {
-	postType := "1"
-	// companyName := "Name"
-	envelope := Envelope{
-		Sec:  "http://xml.amadeus.com/2010/06/Security_v1",
-		Link: "http://wsdl.amadeus.com/2010/06/ws/Link_v1",
-		Ses:  "http://xml.amadeus.com/2010/06/Session_v3",
-		Header: Header{
-			Wsa: "http://www.w3.org/2005/08/addressing",
-			Typ: "http://xml.amadeus.com/2010/06/Types_v1",
-			Iat: "http://www.iata.org/IATA/2007/00/IATA2010.1",
-			AMASecurityHostedUser: AMASecurityHostedUser{
-				UserID: UserID{
-					PosType:        postType,
-					RequestorType:  "U",
-					PseudoCityCode: "HANVN08AA",
-					AgentDutyCode:  nil,
-					RequestorID: RequestorID{
-						CompanyName: nil,
+	envelope := SearchAirMultiAvailabitiy{
+		Env:  &Env,
+		Sec:  &Sec,
+		Link: &Link,
+		Ses:  &Ses,
+		Header: &Header{
+			Wsa: &Wsa,
+			Typ: &Typ,
+			Iat: &Iat,
+			AMASecurityHostedUser: &AMASecurityHostedUser{
+				UserID: &UserID{
+					PosType:        &PosType,
+					RequestorType:  &RequestorType,
+					PseudoCityCode: &PseudoCityCode,
+					AgentDutyCode:  &AgentDutyCode,
+					RequestorID: &RequestorID{
+						CompanyName: &CompanyName,
 					},
 				},
+			},
+			Security: &Security{
+				Wsse: &Wsse,
+				Wsu:  &Wsu,
+				UsernameToken: &UsernameToken{
+					Username: &Username,
+					Password: &Password{
+						Type: &PasswordType,
+						Text: &PasswordText,
+					},
+					Nonce: &Nonce{
+						EncodingType: &EncodingType,
+						Text:         &NonceText,
+					},
+					Created: &Created,
+				},
+			},
+			To:        &To,
+			Action:    &Action,
+			MessageID: &MessageID,
+			TransactionFlowLink: &TransactionFlowLink{
+				Consumer: &Consumer{
+					UniqueID: &UniqueID,
+				},
+			},
+			Session: &Session{
+				TransactionStatusCode: &TransactionStatusCode,
 			},
 		},
 	}
